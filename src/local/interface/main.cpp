fd9aefc42b1fa69373ed2caf442a2bd069aa625d
Adrien Boucaud <adrien.bk@free.fr>
2017-06-04 15:30:33 +0200

    Ajout début opencv

--- src/local/interface/main.cpp
+++ src/local/interface/main.cpp
@@ -1,11 +1,25 @@
 
 #include <QApplication>
+#include <iostream>
 
 #include "myWindow.hpp"
 #include "../../lib/interface/application_qt.hpp"
+#include "opencv2/videoio.hpp"
+#include "opencv2/highgui.hpp"
 
 int main(int argc, char *argv[])
 {
+    cv::VideoCapture capture = cv::VideoCapture(0);
+    if(!capture.isOpened())
+    {
+        std::cerr<<"Failed to open Camera"<<std::endl;
+        exit(1);
+    }
+
+    cv::Mat frame;
+    capture.read(frame);
+    cv::imshow("frame", frame);
+
     srand(time(0));
     //Qt main application (handles exceptions)
     application_qt app(argc, argv);
@@ -15,5 +29,8 @@ int main(int argc, char *argv[])
     win.show();
 
     //Start Qt Application
-    return app.exec();
+    int ret = app.exec();
+    cv::destroyAllWindows();
+
+    return ret;
 }
